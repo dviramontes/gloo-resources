c2bc27b0ae0be6de74e1d959f9ccb6faa2336372
David Viramontes <dviramontes@gmail.com>
2017-10-30 13:30:26 -0600

    feat: begins row component

--- src/cljs/gloo_resources/table.cljs
+++ src/cljs/gloo_resources/table.cljs
@@ -24,10 +24,27 @@
            "startDate"
            "re-assign"])
 
+(defn row [contents]
+  [:tr {:class "striped--light-gray"}
+   [:td {:class "pv2 ph3"} contents]
+   [:td {:class "pv2 ph3"} contents]
+   [:td {:class "pv2 ph3"} contents]])
+
 (defn table []
   (rf/dispatch [:fetch-graph db-key query])
   (let [resources-subs (rf/subscribe [db-key])]
     (fn []
-      [:div#table "test"])))
+      (prn @resources-subs)
+      [:div#table-container
+       [:table {:class "collapse ba br2 b--black-10 pv2 ph3"}
+        [:tbody
+         [:tr {:class "striped--light-gray"}
+          [:th {:class "pv2 ph3 tl f6 fw6 ttu"} "NAME"]
+          [:th {:class "tr f6 ttu fw6 pv2 ph3"} "END_DATE"]
+          [:th {:class "tr f6 ttu fw6 pv2 ph3"} "URL"]]
+         [row "test"]]]])))
+
+
+
 
 
