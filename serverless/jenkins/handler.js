b7516068c7bce59c8a7f005c3d59d08a324cc1fd
David Viramontes <dviramontes@gmail.com>
2017-11-05 01:16:48 -0700

    feat: assign resource type

--- serverless/jenkins/handler.js
+++ serverless/jenkins/handler.js
@@ -1,15 +1,31 @@
 'use strict';
 
-const urlTest = /polymer-qa[0-9]?[0-9]?\.dev\.gloo\.us|polymer-restricted[0-9]?[0-9]?\.dev\.gloo\.us|polymer-uitest[0-9]?[0-9]?\.dev\.gloo\.us/
+const qaTest = /polymer-qa[0-9]?[0-9]?\.dev\.gloo\.us/;
+const reTest = /polymer-restricted[0-9]?[0-9]?\.dev\.gloo\.us/;
+const uiTest = /polymer-uitest[0-9]?[0-9]?\.dev\.gloo\.us/;
+
+const matchAnyTest = (name) => qaTest.test(name) || reTest.test(name) || uiTest.test(name);
 
 module.exports.jenkinsInfo = (event, context, callback) => {
     const jenkins = require('jenkins')({
         baseUrl: process.env.GLOO_RESOURCE_ALLOC_JENKINS_URL,
         crumbIssuer: true
     });
-    jenkins.info((err, { jobs }) => {
+    jenkins.info((err, {jobs}) => {
         if (err) throw new Error(err);
-        const filteredResources = jobs.filter(({ name }) => urlTest.test(name));
-        callback(null, filteredResources); // uses lambda proxy integration
+        const filteredResources = jobs
+            .filter(({name}) => matchAnyTest(name))
+            .map((resource) => {
+                const {name} = resource;
+                if (qaTest.test(name)) {
+                    resource.type = 'qa';
+                } else if (reTest.test(name)) {
+                    resource.type = 're';
+                } else {
+                    resource.type = 'ui';
+                }
+                return {[resource.name]: resource};
+            });
+        return callback(null, filteredResources); // uses lambda proxy integration
     });
 };
