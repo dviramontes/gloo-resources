b2c133d0e7cdf88ad10cf9a89d1d621de7231d78
David Viramontes <dviramontes@gmail.com>
2017-11-23 13:51:23 -0500

    reset: server.clj

--- src/clj/gloo_resources/server.clj
+++ src/clj/gloo_resources/server.clj
@@ -2,7 +2,8 @@
   (:require [gloo-resources.handler :refer [handler]]
             [config.core :refer [env]]
             [ring.adapter.jetty :refer [run-jetty]])
+  (:gen-class))
 
- (defn -main [& args]
-   (let [port (Integer/parseInt (or (env :port) "5000"))]
-     (run-jetty handler {:port port :join? false}))))
+(defn -main [& args]
+  (let [port (Integer/parseInt (or (env :port) "3000"))]
+    (run-jetty handler {:port port :join? false})))
